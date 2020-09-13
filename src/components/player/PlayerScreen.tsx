9cfb4db764b0d929462feece5f3bca9f2c951f41
Casneil <casneil.simpson@gmail.com>
2020-09-13 15:43:09 +0200

    Adding PlayerScreen component

---
+++ src/components/player/PlayerScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Box, Text } from "react-native-design-utility";
+
+const PlayerScreen = () =>{
+    return (
+        <Box f={1} bg="white">
+            <Text>Player Screen</Text>
+        </Box>
+    )
+};
+
+export default PlayerScreen;
